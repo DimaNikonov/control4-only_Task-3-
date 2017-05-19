2cf30604e76334c87c6e6fcad56c53fb6c6e83d4
Валентина <@VAL>
2017-05-19 23:06:13 +0300

    Add project files.

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> collection = new List<int> { 55, 14, 77 ,33, 23, 20, 13, 32 };
+            Console.WriteLine(string.Join(" ",collection.OrderBy(t=> collection.Max(x=>t/10))));
+        }
+    }
+}
